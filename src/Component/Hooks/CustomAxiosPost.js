d338bd255466732d628680c4429259ae31ae82d7
ZH-Jihan <zakirhossain19920@gmail.com>
2024-03-11 17:45:31 +0600

    fix3

--- src/Component/Hooks/CustomAxiosPost.js
+++ src/Component/Hooks/CustomAxiosPost.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 const testUrl = `http://localhost:5000/api/v1`
     const mainUrl = `https://pu-server-1.onrender.com/api/v1`
-    
-
-const CustomAxiosPost = async (url, data) => {
     const token = JSON.parse(localStorage.getItem('authToken'))?.token
     
     const headers = {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${token}`,
     };
+
+const CustomAxiosPost = async (url, data) => {
+    
     try {
         const response = await axios.post(`${mainUrl}${url}`, JSON.stringify(data), {headers});
         return response.data;
