e3fba2bd9cfc397866ffaaaedfbf8f2bac136215
ZH-Jihan <zakirhossain19920@gmail.com>
2024-05-21 15:57:19 +0600

    add filter btn in report

--- src/Component/Page/All Report/TutionFee.js
+++ src/Component/Page/All Report/TutionFee.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useLocalDataFatch from '../../Hooks/localDataFatch';
 import Table from '../../Utilits/Table/Table';
 
@@ -24,13 +24,32 @@ const TutionFee = () => {
         { field: "2008", header: "" },
         { field: "2007", header: "" },
       ];
+      const [selectedName, setSelectedName] = useState('');
+    const uniqueNames = Array.from(new Set(tutionfeeData.map(item => item.department)));
+    const handleSelectChange = (event) => {
+        setSelectedName(event.target.value);
+    };
+    let data;
+    if (selectedName) {
+        data = tutionfeeData.filter(item => item.department === selectedName);
+    } else {
+        data = tutionfeeData
+    }
     return (
         <div>
             <h1 className="text-center font-bold text-3xl my-4">
             PU - Per Credit Fees
       </h1>
-
-            <Table columns={tableHead} data={tutionfeeData}/>
+      <div class="form-control lg:ml-16 lg:w-1/6 flex flex-col mb-3">
+            <label class="font-semibold text-gray-600 py-2">Select Program</label>
+            <select class="appearance-none text-base block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-4" value={selectedName} onChange={handleSelectChange}>
+                <option value="">--- Select ---</option>
+                {uniqueNames.map(name => (
+                    <option key={name} value={name}>{name}</option>
+                ))}
+            </select>
+            </div>
+            <Table columns={tableHead} data={data}/>
         </div>
     );
 };
